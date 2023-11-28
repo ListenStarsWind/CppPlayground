50012b2f2b4b246cf14d7dd7317c0af67d1b50d7
listen-to-the-wind-ruoyi <baotingxi7958@163.com>
2023-11-28 16:20:01 +0800

    上课时的实验板书

---
+++ proiect_2023_11_28/project_2023_11_28.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a = 0;
+    int b = 0;
+    float c = 0.0f;
+    float d = 0.0f;
+    scanf("%d%d%f%f",&a,&b,&c,&d);
+    printf("%d\n",a);
+    printf("%d\n",b);
+    printf("%f\n",c);
+    printf("%f\n",d);
+    return 0;
+}
