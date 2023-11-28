a2ca59bf5fb99e7293f01042c9ddfccc930d3d5f
listen-to-the-wind-ruoyi <baotingxi7958@163.com>
2023-11-28 22:02:45 +0800

    据说智商140以上者称为天才，KiKi想知道他自己是不是天才，请帮他编程判断。输入一个整数表示一个人的智商，如果大于等于140，则表明他是一个天才，输出“Genius”。

---
+++ project_2023_11_28_1/project_2023_11_28_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() 
+{
+    int a = 0;
+    while (scanf("%d", &a) == 1) {
+        if (a >= 140) {
+            char arr[] =  "Genius";
+            char* p;
+            p = arr;
+            printf("%s", p);
+        }
+    }
+
+    return 0;
+}
