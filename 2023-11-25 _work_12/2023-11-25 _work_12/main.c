18219b4b87090ef5720f1b94bd17d6ce75c44d05
listen-to-the-wind-ruoyi <baotingxi7958@163.com>
2023-12-08 22:39:14 +0800

    如题

--- 2023-11-25 _work_12/2023-11-25 _work_12/main.c
+++ 2023-11-25 _work_12/2023-11-25 _work_12/main.c
@@ -4,13 +4,14 @@ int main()
 {
 
 	int i = 0;
-	int j = 0;
+	int j = 1;
 	float k = 0.0;
 	float a = 1.0;
-	for (i = 0; i < 50; i++)
+	for (i = 0; i < 99; i++)
 	{
-		k += 1 / a - 1 / (a + 1);
-		a += 2;
+		k += 1 / a * j;
+		a++;
+		j = -j;
 	}
 	printf("%f", k);
 	return 0;
