2d15f82a2acc62ac79cd7c562813e32131a18cfc
listen-to-the-wind-ruoyi <baotingxi7958@163.com>
2023-12-27 18:20:13 +0800

    让打印效果淡出。

---
+++ 课程9板书代码-2/课程9板书代码-2/main.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+
+int main()
+{
+	int i = 0;
+	int left = 0;
+	int right = 0;
+	int a = 0;
+	char arr1[] = { "****Hello Word****" };
+	char arr2[] = { "******************" };
+	i = sizeof(arr1) / sizeof(arr1[0]);
+	for (left = 0, right = i - 2; left <= right; left++, right--)
+	{
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		for (a = 0; a < i; a++)
+			printf("%c", arr2[a]);
+		Sleep(1000);
+		system("cls");
+	}
+	for (a = 0; a < i; a++)
+		printf("%c", arr2[a]);
+	return 0;
+}
