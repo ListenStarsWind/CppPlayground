a51a45c43a177138e66ce0e4dd14220768bd0c9e
listen-to-the-wind-ruoyi <baotingxi7958@163.com>
2024-03-14 16:14:28 +0800

    还是看注释，再看看这天之前提交的代码注释，有联系的。

--- Project_2024_3_14/Project_2024_3_14/main.c
+++ Project_2024_3_14/Project_2024_3_14/main.c
@@ -1,8 +1,6 @@
-//"*"号打印，这个主要是找规律：
-//比方说我们输入一个数字5，那就可以认为创建了一个5*5的棋盘，编号是01234然后我们看到这个“*”号
-//实际是在对角线上显示的，如果把i看做行数，j看做列数就可以认为，“*”号会在i==j以及i+j==4上出现：
-//题目链接：https://www.nowcoder.com/share/jump/9209693051710402272732
 #define _CRT_SECURE_NO_WARNINGS
+//像这种图案打印题，一定要画画图，否则很难找的规律的，你可以看下同一天之前提交的代码注释联系一下；
+//题目：https ://www.nowcoder.com/share/jump/9209693051710403447399
 #include<stdio.h>
 int main()
 {
@@ -13,20 +11,15 @@ int main()
 		{
 			for (j = 0; j < import; j++)
 			{
-				if (i == j)
-				{
-					printf("*");
-				}
-				else if (i + j == import - 1)
-				{
-					printf("*");
-				}
+				if (i == 0 || i == import - 1)
+					printf("* ");
+				else if (j == 0 || j == import - 1)
+					printf("* ");
 				else
-					printf(" ");
+					printf("  ");
 			}
 			printf("\n");
 		}
 	}
-
 	return 0;
 }
