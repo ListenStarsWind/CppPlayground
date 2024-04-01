3ad65e977c520d080fb88d02ba8f0a90ac20b687
listen-to-the-wind-ruoyi <baotingxi7958@163.com>
2024-04-01 14:32:47 +0800

    注释

---
+++ 2024-01-21_debug和release的区别等_作业/第八题/第八题/main.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 
+//求Sn = a + aa + aaa + aaaa + aaaaa的前5项之和，其中a是一个数字，
+//
+//例如：2 + 22 + 222 + 2222 + 22222
+//
+#include<math.h>
+#include<stdio.h>
+
+int calculate(int m)
+{
+	int ret = 0;
+	int j;
+	int k = m;
+	for (j = 1; j <= 5; j++)
+	{
+		ret += m;
+		m += pow(10, j) * k;
+	}
+	return ret;
+
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	printf("%d", calculate(n));
+	return 0;
+}
