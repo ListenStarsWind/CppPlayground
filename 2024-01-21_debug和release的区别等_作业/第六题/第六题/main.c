eb6e2f17302d5400087f503c469b1e4b46d06b98
listen-to-the-wind-ruoyi <baotingxi7958@163.com>
2024-04-01 14:31:06 +0800

    作业，看注释。

---
+++ 2024-01-21_debug和release的区别等_作业/第六题/第六题/main.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+void print(int n)
+{
+	int i, j, k;
+	for (i = 0; i < n; i++)
+	{
+		k = i;
+		if (k <= (n - 1) / 2)
+			k = k;
+		else
+			k = (n - 1) - k;
+		for (j = 0; j < n; j++)
+		{
+			if (j<(n - 1) / 2 - k || j>(n - 1) / 2 + k)
+				printf("  ");
+			else
+				printf("* ");
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	print(n);
+	return 0;
+}
