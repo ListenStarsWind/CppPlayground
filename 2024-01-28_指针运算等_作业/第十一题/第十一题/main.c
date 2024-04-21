d0589a09e077eb7b752225cd54531aa2fbee17ec
listen-to-the-wind <baotingxi7958@163.com>
2024-04-21 20:57:35 +0800

    看注释

--- 2024-01-28_指针运算等_作业/第十一题/第十一题/main.c
+++ 2024-01-28_指针运算等_作业/第十一题/第十一题/main.c
@@ -4,13 +4,18 @@
  * 【文件标识】：
  * ========================================================================================
  * 【当前作者】：	听风若依
- * 【当前版本】：	V1.0
- * 【完成日期】：	2024年  月日
- * 【更新内容】：
+ * 【当前版本】：	V1.2
+ * 【完成日期】：	2024年  4月日
+ * 【更新内容】：修复了不能多次输入的BUG，以及三个方法连续调用的字符串复位问题
  * ========================================================================================
  * 【历史作者】：	听风若依
- * 【历史版本】：	V1.
- * 【历史日期】：	2024年
+ * 【历史版本】：	V1.1
+ * 【完成日期】：	2024年  4月16日
+ * 【更新内容】：优化了底层运行逻辑，更正了错误理解
+ * ========================================================================================
+ * 【历史作者】：	听风若依
+ * 【历史版本】：	V1.0
+ * 【历史日期】：	2024年  4月9日
  * ========================================================================================
  * 【开发平台】：	VS2022
  * 【程序内容】：实现一个函数，可以左旋字符串中的k个字符。
@@ -18,10 +23,223 @@
  *******************************************************************************************/
 #include<stdio.h>
 #include<assert.h>
+#include<string.h>
 
- //spin_1输入字符串地址，左旋数目；
- //注意字符串长度不得超过100；
- //若左旋数目超出字符串长度，则返回EOF。
+
+
+ //共有三种方法。
+
+ //方法一：逐个旋转，可以看下面举的例子，abcd左旋2位得到cdab,cdab也可以由bcda左旋1位得到
+//方法一 字符串的大小没有限制
+void spin_left1(char str[], int n)
+{
+	int len = strlen(str);//放心使用strlen()不管是数组还是指针都可以使用，且包含空格，不包含‘\0’
+	int logic = n % len;//计算逻辑旋转次数
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < logic; i++)
+	{
+		char push = str[0];
+		for (j = 0; j < len - 1; j++)
+		{
+			str[j] = str[j + 1];
+		}
+		str[len - 1] = push;
+	}
+}
+
+//方法二：调用两个库函数 strcpy 和 strcant(或者说是strncat)
+//strcpy的形参是两个储存字符串的载体，它会把后一个载体中的字符串赋给前一个字符串的载体（比如数组，指针），用来拷贝字符串
+//例如：
+//#include<string.h>  strcpy头文件
+//int main()
+//{
+//	char arr1[20] = "hello word";
+//	char arr2[20] = "I am a student";
+//	strcpy(arr1, arr2);
+//	printf("%s", arr1);
+//	return 0;
+//}
+//cmd显示：I am a student
+
+//strncat是strcat的变形，strcat的形参也是两个字符型数组，如果把strcpy比作赋值号“=”，那strcat就相当于“+=”，用来拼接字符串
+//#include<string.h>  strcat头文件
+//int main()
+//{
+//	char arr1[30] = "hello word";
+//	char* str = "I am a student";
+//	strcat(arr1, str);
+//	printf("%s", arr1);
+//	return 0;
+//}
+//cmd显示：hello wordI am a student
+
+//strcat是完全拼接，而strncat是不完全拼接，相比strcat,strncat多了一个整型形参，用来描述不完全的程度
+//#include<string.h>  strncat头文件
+//int main()
+//{
+//	char arr1[30] = "hello word";
+//	char* str = "I am a student";
+//	strncat(arr1, str, 4);
+//	printf("%s", arr1);
+//	return 0;
+//}
+//cmd显示：hello wordI am     I am的长度不就是四吗？
+//需要注意的是使用它们都需要注意数组大小，比如若将第二个示例main函数中的30改为20，arr1就存不下hello wordI am a student了
+
+//方法二 有字符串的大小限制
+void spin_left2(char str[], int n)
+{
+	int len = strlen(str);//放心使用strlen()不管是数组还是指针都可以使用，且包含空格，不包含‘\0’
+	int logic = n % len;//计算逻辑旋转次数
+	char str_push[256] = { '0' };
+	strcpy(str_push, str + logic);//数组名不就是地址吗？如果str中存的是abcd，那么str+2不就是cd吗
+	strncat(str_push, str, logic);
+	strcpy(str, str_push);
+}
+
+//第三种方法：三段倒置法
+//还是对abcd来说
+//右旋2位就是
+//先倒置前两位：ba
+//再倒置后面的：dc
+//现在变成badc
+//最后再整体倒置：cdab
+
+//下面是倒置函数：
+//start是开始下标，ending是结束下标
+void changeover(char str[], int start, int ending)
+{
+	int left = start;
+	int right = ending;
+	for (; left < right; left++, right--)
+	{
+		char tmp = str[left];
+		str[left] = str[right];
+		str[right] = tmp;
+	}
+}
+
+//下面是方法三本体
+//方法三字符串长度没有限制
+void spin_left3(char str[], int n)
+{
+	int len = strlen(str);//放心使用strlen()不管是数组还是指针都可以使用，且包含空格，不包含‘\0’
+	int logic = n % len;//计算逻辑旋转次数
+	changeover(str, 0, logic - 1);
+	changeover(str, logic, len - 1);
+	changeover(str, 0, len - 1);
+}
+
+//库函数fgets有三个参数，第一个参数是用来储存字符串的载体，第二个参数限制读取的字符串长度，第三个参数规定字符串的输入方式，
+//若是用键盘读取或者说从缓冲区中读取，则是stdin（标准输入流）。参考资料：http://t.csdnimg.cn/XF4Z4
+int main()
+{
+	void (*p[4])(char[], int) = { 0, spin_left1, spin_left2, spin_left3 };
+	char arr[256] = { '0' };
+	int mov = 0;
+	char buffer[256]; // 改动：用于读取多余的输入
+	while ((printf("请输入字符串，注意字符串长度不要超过256个字节:\n") > 0) && (fgets(arr, 256, stdin) != NULL)
+		&& (printf("请输入右旋位数：\n") > 0) && (scanf("%d", &mov) == 1))
+	{
+		fgets(buffer, sizeof(buffer), stdin); // 清除多余的输入
+		size_t len = strlen(arr);
+		if (arr[len - 1] == '\n') {
+			arr[len - 1] = '\0';
+		}
+
+		int i = 0;
+		char arr_reset[256] = { '0' };
+		strcpy(arr_reset, arr);
+		for (i = 1; i < 4; i++)
+		{
+			switch (i)
+			{
+			case 1:
+				printf("下面是第一种方法：\n");
+				(*p[i])(arr, mov);
+				printf("右旋结果：\n%s\n", arr);
+				strcpy(arr, arr_reset);
+				break;
+			case 2:
+				printf("下面是第二种方法：\n");
+				(*p[i])(arr, mov);
+				printf("右旋结果：\n%s\n", arr);
+				strcpy(arr, arr_reset);
+				break;
+			case 3:
+				printf("下面是第三种方法：\n");
+				(*p[i])(arr, mov);
+				printf("右旋结果：\n%s\n", arr);
+				strcpy(arr, arr_reset);
+				break;
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
+#if 0
+============================================================================================ =
+下面是V1.1的main函数部分
+//主函数
+int main()
+{
+	void (*p[4])(char[], int) = { 0, spin_left1, spin_left2, spin_left3 };
+	char arr[256] = { '0' };
+	int mov = 0;
+	while ((0 < printf("请输入字符串，注意字符串长度不要超过256个字节:\n")) && (EOF != gets(arr))
+		&& (0 < printf("请输入右旋位数：\n")) && (scanf("%d", &mov)))
+	{
+		int i = 0;
+		char arr_reset[256] = { '0' };
+		strcpy(arr_reset, arr);
+		for (i = 1; i < 4; i++)
+		{
+			switch (i)
+			{
+			case 1:
+				printf("下面是第一种方法：\n");
+				(*p[i])(arr, mov);
+				printf("右旋结果：\n%s\n", arr);
+				strcpy(arr, arr_reset);
+				break;
+			case 2:
+				printf("下面是第二种方法：\n");
+				(*p[i])(arr, mov);
+				printf("右旋结果：\n%s\n", arr);
+				strcpy(arr, arr_reset);
+				break;
+			case 3:
+				printf("下面是第三种方法：\n");
+				(*p[i])(arr, mov);
+				printf("右旋结果：\n%s\n", arr);
+				strcpy(arr, arr_reset);
+				break;
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+============================================================================版本分隔符
+错误理解指的是1.0我认为必须旋转少于字符串长度的数，但实际是可以超过：
+举个例子：对于abcd，
+左旋1位是bcda
+左旋2位是cdab
+左旋3位是dabc
+左旋4位是abcd
+左旋5位是bcda
+左旋6位是cdab
+左旋7位是dabc
+
+V1.0内容：
+//spin_1输入字符串地址，左旋数目；
+//注意字符串长度不得超过100；
+//若左旋数目超出字符串长度，则返回EOF。
 int spin_l(char* p_import, int move)
 {
 	assert(p_import != NULL);//野指针断言
@@ -75,3 +293,4 @@ int main()
 	printf("请检查您的输入是否有误，再重新运行程序");//请勿使用UTF-8汉字编码
 	return 0;
 }
+#endif
---
+++ 2024-01-28_指针运算等_作业/第十三题/第十三题/main.c
@@ -0,0 +1,274 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+/******************************************************************************************
+ * 【文件名称】：	main.c
+ * 【文件标识】：
+ * ========================================================================================
+ * 【当前作者】：	听风若依
+ * 【当前版本】：	V1.1
+ * 【完成日期】：	2024年  4月19日
+ * 【更新内容】：优化掉了多余功能，减轻了硬件负担
+ * ========================================================================================
+ * 【历史作者】：	听风若依
+ * 【历史版本】：	V1.0
+ * 【历史日期】：	2024年  4月10日
+ * ========================================================================================
+ * 【开发平台】：	VS2022
+ * 【程序内容】： 输入一个整数数组，实现一个函数，
+				来调整该数组中数字的顺序使得数组中所有的奇数位于数组的前半部分，
+				所有偶数位于数组的后半部分。
+*******************************************************************************************/
+#include<stdio.h>
+#include<assert.h>
+
+//排序函数
+int sort(int* const p, int sz)//const修饰指针变量，防止其指向位置发生变化
+{
+	assert(p != NULL);//断言野指针
+	int* p_agency = p;//定义代理指针，为可能的直接性地址改变做准备
+	int* left = p;
+	int* right = p + sz - 1;
+	while (left < right)
+	{
+		while (*left % 2 != 0)
+		{
+			left++;
+		}
+		while (*right % 2 == 0)
+		{
+			right--;
+		}
+		int tmp = 0;
+		tmp = *(left);
+		*(left) = *(right);
+		*(right) = tmp;
+	}
+}
+
+
+//数组初始化函数
+int input(int* const p_import, int sz)//const修饰指针变量，防止其指向位置发生变化
+{
+	assert(p_import != NULL);//野指针断言
+	int* p = p_import;//数据预复位
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < sz; i++)
+	{
+		int push = 0;
+		push = scanf("%d", (p + i));
+		if (push == EOF)
+		{
+			return EOF;//输入失败，返回EOF
+		}
+		else
+		{
+			j += push;
+		}
+	}
+	return j;//正常运行返回读取个数
+}
+
+//数组打印函数
+int print(int* const p_import, int sz)//const修饰指针变量，防止其指向位置发生变化
+{
+	assert(p_import != NULL);//野指针断言
+	int* p = p_import;//指针预复位
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < sz; i++)
+	{
+		int push = 0;
+		push = printf("%d ", *(p + i));
+		if (push < 0)
+		{
+			return EOF;//打印失败，返回EOF
+		}
+		else
+		{
+			j += push;
+		}
+	}
+	printf("\n");
+	return j;//正常运行返回输出个数
+}
+
+
+//主函数
+//请勿使用UTF-8汉字编码
+int main()
+{
+	int arr[10] = { 0 };
+	int sz = 0;
+	int push = 0;
+	int i = 0;
+	sz = sizeof(arr) / sizeof(arr[0]);
+	int(*p[4])(int*, int) = { 0, input, sort, print };
+	while (push != EOF)
+	{
+		i = 1;
+		int x = 0;
+		int y = 0;
+		int z = 0;
+		for (i = 1; i < 4; i++)
+		{
+			switch (i)
+			{
+			case 1:
+				printf("请输入数组元素）(10个元素)：\n");
+				x = (*p[i])(arr, sz);
+				break;
+			case 2:
+				y = (*p[i])(arr, sz);
+				break;
+			case 3:
+				printf("这是排列后的数组：\n");
+				z = (*p[i])(arr, sz);
+				break;
+			}
+			if (x == EOF || y == EOF || z == EOF)
+			{
+				push = EOF;
+			}
+		}
+		printf("\n");
+
+	}
+	return 0;
+}
+
+
+#if 0
+V1.0内容：
+#include<stdio.h>
+#include<assert.h>
+
+//排序函数
+int sort(int* const p_import, int sz)//const修饰指针变量，防止其指向位置发生变化
+{
+	assert(p_import != NULL);//野指针断言
+	int* p = p_import;
+	int i = 0;
+	int j = 0;
+	int count = 0;
+	for (i = 0; i < sz; i++)//遍历数组，检查偶数个数
+	{
+		if (*(p + i) % 2 == 0)
+		{
+			count++;
+		}
+	}
+	while (count)
+	{
+		int store = 0;
+		for (i = j; i < sz; i++)//遍历数组，找出并储存偶数
+		{
+			if (*(p + i) % 2 == 0)
+			{
+				j = i;//数据复位
+				store = *(p + i);
+				count--;
+				break;
+			}
+		}
+		while (i < sz - 1)//将之后元素向前位
+		{
+			*(p + i) = *(p + i + 1);
+			i++;
+		}
+		*(p + sz - 1) = store;//把偶数放置在最后
+	}
+	return 0;//代表程序正常返回
+}
+
+//数组初始化函数
+int input(int* const p_import, int sz)//const修饰指针变量，防止其指向位置发生变化
+{
+	assert(p_import != NULL);//野指针断言
+	int* p = p_import;//数据预复位
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < sz; i++)
+	{
+		int push = 0;
+		push = scanf("%d", (p + i));
+		if (push == EOF)
+		{
+			return EOF;//输入失败，返回EOF
+		}
+		else
+		{
+			j += push;
+		}
+	}
+	return j;//正常运行返回读取个数
+}
+
+//数组打印函数
+int print(int* const p_import, int sz)//const修饰指针变量，防止其指向位置发生变化
+{
+	assert(p_import != NULL);//野指针断言
+	int* p = p_import;//指针预复位
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < sz; i++)
+	{
+		int push = 0;
+		push = printf("%d ", *(p + i));
+		if (push < 0)
+		{
+			return EOF;//打印失败，返回EOF
+		}
+		else
+		{
+			j += push;
+		}
+	}
+	printf("\n");
+	return j;//正常运行返回输出个数
+}
+
+
+//主函数
+//请勿使用UTF-8汉字编码
+int main()
+{
+	int arr[10] = { 0 };
+	int sz = 0;
+	int push = 0;
+	int i = 0;
+	sz = sizeof(arr) / sizeof(arr[0]);
+	int(*p[4])(int*, int) = { 0, input, sort, print };
+	while (push != EOF)
+	{
+		i = 1;
+		int x = 0;
+		int y = 0;
+		int z = 0;
+		for (i = 1; i < 4; i++)
+		{
+			switch (i)
+			{
+			case 1:
+				printf("请输入数组元素）(10个元素)：\n");
+				x = (*p[i])(arr, sz);
+				break;
+			case 2:
+				y = (*p[i])(arr, sz);
+				break;
+			case 3:
+				printf("这是排列后的数组：\n");
+				z = (*p[i])(arr, sz);
+				break;
+			}
+			if (x == EOF || y == EOF || z == EOF)
+			{
+				push = EOF;
+			}
+		}
+		printf("\n");
+		/*char c_tmp;
+		while ((c_tmp = getch() != '\n')  && c_tmp != EOF);*/ //不要清除缓冲区会卡死
+	}
+	return 0;
+}
+#endif
