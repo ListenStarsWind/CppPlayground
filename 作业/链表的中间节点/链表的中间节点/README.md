55e97a27b33e5c4edef07adb5a53f076aa2053b2
听风若依 <baotingxi7958@163.com>
2024-07-30 16:19:02 +0800

    nothing

---
+++ 作业/链表的中间节点/链表的中间节点/README.md
@@ -0,0 +1,7 @@
+# [链表的中间节点](https://leetcode.cn/problems/middle-of-the-linked-list/description/)
+
+![image-20240730153450573](https://md-wind.oss-cn-nanjing.aliyuncs.com/md/202407301534838.png)
+
+这个真的很简单，好吧，希望重写的时候不会和上个项目《链表删除指定节点》那样打脸。没有打脸，倒是力扣的统计，之前就说过力扣统计有问题，现在来看个离谱的：
+
+![image-20240730161537354](https://md-wind.oss-cn-nanjing.aliyuncs.com/md/202407301615536.png)
--- 作业/链表的中间节点/链表的中间节点/main.c
+++ 作业/链表的中间节点/链表的中间节点/main.c
@@ -1,1 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+typedef struct ListNode ListNode;
+typedef ListNode* PListNode;
+struct ListNode* middleNode(struct ListNode* head) {
+	PListNode PFast = head;
+	PListNode PSlow = head;
+	while (PFast && PFast->next)
+	{
+		PFast = PFast->next->next;
+		PSlow = PSlow->next;
+	}
+	return PSlow;
+}
